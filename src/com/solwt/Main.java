8157abf7280afad446e3a64a082c44fa807bb342
haroun19010 <haroun19010@hotmail.com>
2020-10-03 16:38:06 +0300

    Initial commit : Exploring Java .... " If statment"

---
+++ src/com/solwt/Main.java
@@ -0,0 +1,17 @@
+package com.solwt;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int temp = 22;
+
+    if (temp > 30)
+        System.out.println("Its a Hot Day"+"\n Drink Water");
+    else if ( temp >20 && temp <30)
+        System.out.println("Beatuful Day");
+    else
+        System.out.println("Cold Day");
+    }
+}
